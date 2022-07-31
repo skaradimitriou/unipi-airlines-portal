e132dfd532c22c0618f492214c07daabd98f864e
Stathis Karadimitriou <64270931+skaradimitriou@users.noreply.github.com>
2022-07-31 16:05:40 +0300

    Update my_db_credentials.php

--- database/my_db_credentials.php
+++ database/my_db_credentials.php
@@ -5,13 +5,30 @@
      */
 
 
-    //db login data
-    define('DB_PASS','postgres');
-    define('DB_USER','postgres'); 
+    /* DEV
 
-    //db name defined in postgresql
-    define('DB_BASE','airlinesFinal');
+        //db login data
+        define('DB_PASS','postgres');
+        define('DB_USER','postgres'); 
 
-    //db server
-    define('DB_SERVER','localhost');
+        //db name defined in postgresql
+        define('DB_BASE','airlinesFinal');
+
+        //db server
+        define('DB_SERVER','localhost');
+
+    */
+
+    /* PROD (Heroku server for portfolio) */
+
+        //db login data
+        define('DB_PASS','1d36b5d84c3c00e0e9079bad870ad35b7a80d122bfc707ff8b9a78f59ef8b6dc');
+        define('DB_USER','sxffoiehwbwffg'); 
+
+        //db name defined in postgresql
+        define('DB_BASE','d63v7k8v3bvjgo');
+
+        //db server
+        define('DB_SERVER','ec2-52-30-159-47.eu-west-1.compute.amazonaws.com');
+
 ?>
