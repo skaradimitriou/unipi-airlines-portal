1eecff25db1251358e59429fdbdd47a9fc661c74
ekaradimitriou <ekaradimitriou@cognity.gr>
2022-08-07 19:00:12 +0300

    Implementation: Site titles & favicon fix

--- details_page/passenger_details.php
+++ details_page/passenger_details.php
@@ -8,8 +8,8 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
 
-        <title>HTML Semester Project - S.Karadimitriou</title>
-        <link rel="icon" type="image/x-icon" href="..img/icons/stathis_airlines_logo.png">
+        <title>Στοιχεία Επιβάτη - Stathis Airlines</title>
+        <link rel="icon" type="image/x-icon" href="img/icons/stathis_airlines_logo_white.ico">
 
         <!-- Allows Media Queries-->
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
